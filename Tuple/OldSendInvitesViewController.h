3cbccd687f51d0e9b72863ebf92c02e6f4706797
Will Gu <willgu29@gmail.com>
2015-06-18 12:42:08 -0700

    Integrating old sendInvites

---
+++ Tuple/OldSendInvitesViewController.h
@@ -0,0 +1,30 @@
+//
+//  SendInvitesViewController.h
+//  Degrees
+//
+//  Created by William Gu on 2/7/15.
+//  Copyright (c) 2015 William Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PullFromContactsList.h"
+#import "BaseTableViewController.h"
+#import "PushToParseCloud.h"
+#import <Parse/Parse.h>
+
+@interface OldSendInvitesViewController : BaseTableViewController <UITableViewDataSource, UITableViewDelegate, PullFromContactsListDelegate, PushToParseCloudDelegate, UITextFieldDelegate>
+
+
+
+// SET BY ANOTHER VC
+@property (nonatomic, strong) NSString *event;
+@property (nonatomic, strong) NSString *location;
+@property (nonatomic, strong) NSString *time;
+//*****
+
+@property (nonatomic, strong) NSMutableArray *displayInfoArray;
+@property (nonatomic, strong) NSMutableArray *cellData;
+@property (nonatomic, strong) NSMutableArray *selectedPeopleArray;
+
+
+@end
